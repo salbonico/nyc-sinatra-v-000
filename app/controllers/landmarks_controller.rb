bb96158c59a2de7936a20d52f861cbfa2cbfd0bf
Steve Albonico <salbonico@gmail.com>
2019-02-12 05:44:48 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -10,4 +10,34 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/new'
   end
 
+  post 'landmarks' do
+    newlandmark = Landmark.create(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year_completed"], :landmark_ids => params["figure"]["landmark_ids"])
+    if params["title"]["name"] != ""
+      if Title.find{|title| title.name == params["title"]["name"]}
+          titleinput = Title.find{|title| title.name == params["title"]["name"]}
+          newfigure.title_ids = titleinput.id
+          newfigure.save
+      else
+          temptitle = Title.create(:name => params["title"]["name"])
+          newfigure.title_ids = temptitle.id
+          newfigure.save
+      end
+    end
+
+    if params["landmark"]["name"] != ""
+      if Landmark.find{|landmark| landmark.name == params["landmark"]["name"]}
+        landmarkinput = Landmark.find{|landmark| landmark.name == params["landmark"]["name"]}
+        newfigure.landmark_ids = landmarkinput.id
+        newfigure.save
+      else
+        templandmark = Landmark.create(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year"])
+        newfigure.landmark_ids = templandmark.id
+        newfigure.save
+      end
+    end
+  id = newfigure.id
+  redirect "/figures/#{id}"
+  end
+
+
 end
