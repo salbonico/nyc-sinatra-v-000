a72f421d27baf05858438e907771da02b1af59b2
Steve Albonico <salbonico@gmail.com>
2019-02-12 06:07:18 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,94 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    newfigure = Figure.create(:name => params["figure"]["name"], :title_ids => params["figure"]["title_ids"], :landmark_ids => params["figure"]["landmark_ids"])
+    if params["title"]["name"] != ""
+      if Title.find{|title| title.name == params["title"]["name"]}
+          titleinput = Title.find{|title| title.name == params["title"]["name"]}
+          newfigure.title_ids = titleinput.id
+          newfigure.save
+      else
+          temptitle = Title.create(:name => params["title"]["name"])
+          newfigure.title_ids = temptitle.id
+          newfigure.save
+      end
+    end
+
+    if params["landmark"]["name"] != ""
+      if Landmark.find{|landmark| landmark.name == params["landmark"]["name"]}
+        landmarkinput = Landmark.find{|landmark| landmark.name == params["landmark"]["name"]}
+        newfigure.landmark_ids = landmarkinput.id
+        newfigure.save
+      else
+        templandmark = Landmark.create(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year"])
+        newfigure.landmark_ids = templandmark.id
+        newfigure.save
+      end
+    end
+  id = newfigure.id
+  redirect "/figures/#{id}"
+end
+
+patch '/figures/:id' do
+  ###bugfix
+if !params.keys.include?("genres")
+    params[:genre] = []
+    end
+###/bugfix
+
+newfigure = Figure.find(params[:id])
+
+newfigure.update(:name => params["figure"]["name"], :title_ids => params["figure"]["title_ids"], :landmark_ids => params["figure"]["landmark_ids"])
+if params["title"]["name"] != ""
+  if Title.find{|title| title.name == params["title"]["name"]}
+      titleinput = Title.find{|title| title.name == params["title"]["name"]}
+      newfigure.title_ids = titleinput.id
+      newfigure.save
+  else
+      temptitle = Title.create(:name => params["title"]["name"])
+      newfigure.title_ids = temptitle.id
+      newfigure.save
+  end
+end
+
+if params["landmark"]["name"] != ""
+  if Landmark.find{|landmark| landmark.name == params["landmark"]["name"]}
+    landmarkinput = Landmark.find{|landmark| landmark.name == params["landmark"]["name"]}
+    newfigure.landmark_ids = landmarkinput.id
+    newfigure.save
+  else
+    templandmark = Landmark.create(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year"])
+    newfigure.landmark_ids = templandmark.id
+    newfigure.save
+  end
+end
+id = newfigure.id
+redirect "/figures/#{id}"
+end
+
+
+
+
+
+get '/figures/:id/edit' do
+@figure = Figure.find(params[:id])
+erb :'/figures/edit'
+end
+
+
+
+get '/figures/:id' do
+@figure = Figure.find(params[:id])
+erb :'/figures/show'
+end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,66 @@
 class LandmarksController < ApplicationController
   # add controller methods
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    newlandmark = Landmark.create(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year_completed"], :figure_id => params["landmark"]["figure_id"])
+    if params["figure"]["name"] != ""
+      if Figure.find{|figure| figure.name == params["figure"]["name"]}
+          figureinput = Figure.find{|figure| figure.name == params["figure"]["name"]}
+          newlandmark.figure_id = figureinput.id
+          newlandmark.save
+      else
+          tempfigure = Figure.create(:name => params["figure"]["name"])
+          newlandmark.figure_id = tempfigure.id
+          newlandmark.save
+      end
+    end
+  id = newlandmark.id
+  redirect "/landmarks/#{id}"
+  end
+
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+
+get '/landmarks/:id' do
+  @landmark = Landmark.find(params[:id])
+  erb :'/landmarks/show'
+end
+
+patch '/landmarks/:id' do
+  ###bugfix
+if !params.keys.include?("genres")
+    params[:genre] = []
+    end
+###/bugfix
+newlandmark = Landmark.find(params[:id])
+newlandmark.update(:name => params["landmark"]["name"], :year_completed => params["landmark"]["year_completed"], :figure_id => params["landmark"]["figure_id"])
+if params["figure"]["name"] != ""
+  if Figure.find{|figure| figure.name == params["figure"]["name"]}
+      figureinput = Figure.find{|figure| figure.name == params["figure"]["name"]}
+      newlandmark.figure_id = figureinput.id
+      newlandmark.save
+  else
+      tempfigure = Figure.create(:name => params["figure"]["name"])
+      newlandmark.figure_id = tempfigure.id
+      newlandmark.save
+  end
+end
+id = newlandmark.id
+redirect "/landmarks/#{id}"
+
+end
+
 end
